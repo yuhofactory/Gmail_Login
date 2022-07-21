b1d532aa99da78a4cab33706be03210358a7d223
Y.U.H.O Official shop <109708975+yuhofactory@users.noreply.github.com>
2022-07-22 02:50:40 +0700

    Buat login py

--- login.py
+++ login.py
@@ -11,19 +11,19 @@ driver.get("https://accounts.google.com/")
 
 #get the username textbox
 login_field = driver.find_element_by_name("identifier")
-login_field.clear()
+login_field.clear() yuhofactory@gmail.com
 
 #enter username
-login_field.send_keys("ENTER USERNAME HERE")
+login_field.send_keys("ENTER USERNAME HERE") yuhofactory@gmail.com
 login_field.send_keys(u'\ue007') #unicode for enter key
 time.sleep(2)
 
 #get the password textbox
-password_field = driver.find_element_by_name("password")
-password_field.clear()
+password_field = driver.find_element_by_name("Mr008800.")
+password_field.clear()Mr008800.
 
 #enter password
-password_field.send_keys("ENTER PASSWORD HERE")
+password_field.send_keys("Mr008800.")
 password_field.send_keys(u'\ue007') #unicode for enter key
 time.sleep(10)
 
